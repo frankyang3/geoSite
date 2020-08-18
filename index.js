65d388b3a11e616269f1365f594f2b853995f310
frankyang3 <frankyang3@gmail.com>
2020-08-17 22:49:37 -0400

    index rotation done

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 //adapted from https://3dtransforms.desandro.com/cube
 
-var lastMouseX = 0,
-	lastMouseY = 0;
+
+
+
+var w = window.innerWidth/2
+var h = window.innerHeight/2;
+
+
+var lastMouseX = w,
+	lastMouseY = h;
 var rotX = 0,
 	rotY = 0;
 
-$(document).on("mousemove", function(ev) {
-	lastMouseX = ev.clientX;
-    lastMouseY = ev.clientY;
-    //console.log(ev.clientX)
+	$( document ).ready(function() {
+	lastMouseX = w;
+	lastMouseY = h;
+	console.log("move")
 	$(document).on("mousemove", mouseMoved);
 });
 
-
-$(document).on("mouseup", function() {
-    console.log("mouseUp")
-	$(document).off("mousemove", mouseMoved);
-});
-
-
 function mouseMoved(ev) {
-	var w = window.innerWidth;
-	var h = window.innerHeight;
+	var deltaX = ev.pageX - lastMouseX;
+	var deltaY = ev.pageY - lastMouseY;
 
-	var deltaX = w/2 - lastMouseX;
-    var deltaY = h/2 - lastMouseY;
-    
-    //console.log("mouseMoved" + deltaX + " lastMouseX" + lastMouseX + ev.pageX)
 	lastMouseX = ev.pageX;
 	lastMouseY = ev.pageY;
 
-	rotY -= deltaX * 0.000001;
-	rotX += deltaY * 0.000001;
+	rotY -= deltaX * 0.1;
+	rotX += deltaY * 0.1;
 
 	$("#cube").css("transform", "translateZ( -100px) rotateX( " + rotX + "deg) rotateY(" + rotY + "deg)");
 }
-
